9dda086474be26714690da6f408c0fa5a5245e05
AlexeyZhuravlev <aa_zhur@mail.ru>
2019-02-28 19:27:50 +0300

    Add boilerplate

---
+++ include/common.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <algorithm>
+#include <vector>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <cassert>
+#include <cmath>
+#include <string>
+#include <queue>
+#include <set>
+#include <map>
+#include <cstdlib>
+
+using namespace std;
+
+struct Context {
+    using TSolution = int;
+
+    void Input() {
+        
+    }
+
+    void Stats() {
+        
+    }
+
+
+    void Output() {
+		
+    }
+
+    void Verify() {
+        
+    }
+
+
+    int GetScore() {
+        
+    }
+
+};
