4e90e9ed44b02a2b8a2a7947ce826d0b0cee3c1c
Sarkin <kinan.sarmini@gmail.com>
2019-02-28 21:06:39 +0300

    common.h

--- include/common.h
+++ include/common.h
@@ -12,32 +12,86 @@
 #include <set>
 #include <map>
 #include <cstdlib>
+#include <unordered_map>
 
 using namespace std;
 
+struct Slide {
+    enum Orient { VER, HOR };
+
+    Orient orient;
+
+    int ntags = 0;
+    vector<int> tags;
+};
+
 struct Context {
-    using TSolution = int;
+    using TSolution = vector<pair<int, int>>;
+
+    int n;
+    vector<Slide> slides;
+    TSolution solution;
 
     void Input() {
-        
+        cin >> n;
+
+        unordered_map<string, int> tag_to_id;
+        for (int i = 0; i < n; i++) {
+            char c;
+            cin >> c;
+            assert(c == 'V' || c == 'H');
+
+            int ntags;
+            cin >> ntags;
+
+            vector<int> tags;
+            for (int j = 0; j < ntags; j++) {
+                string s;
+                cin >> s;
+                auto it = tag_to_id.find(s);
+                int id = -1;
+                if (it == tag_to_id.end()) {
+                    id = tag_to_id.size();
+                    tag_to_id.emplace(s, id);
+                } else {
+                    id = it->second;
+                }
+                assert(id >= 0);
+                tags.push_back(id);
+            }
+
+            Slide s{(c == 'V')? Slide::VER : Slide::HOR, ntags, tags};
+            slides.emplace_back(std::move(s));
+        }
     }
 
     void Stats() {
-        
     }
 
 
     void Output() {
-		
+        cout << solution.size() << endl;
+        for (auto& p : solution) {
+            if (p.second < 0) {
+                cout << p.first << endl;
+            } else {
+                cout << p.first << " " << p.second << endl;
+            }
+        }
     }
 
     void Verify() {
-        
+        vector<int> used(n, 0);
+        for (auto& p : solution) {
+            assert(p.first >= 0 && p.second >= -1);
+            assert(p.first < n && p.second < n);
+            assert(!used[p.first] && !used[p.second]);
+            used[p.first] = used[p.second] = 1;
+        }
     }
 
 
     int GetScore() {
-        
     }
 
 };
